dfd60a95e7ad83e49bc498d4e363470c272d2d2b
Leandro Oliveira Silva <loliveiras@minsait.com>
2024-12-10 18:38:52 -0300

    Adicionando casca do microservico

---
+++ mini-autorizador/src/test/java/br/com/miniautorizador/mini_autorizador/MiniAutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miniautorizador.mini_autorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
